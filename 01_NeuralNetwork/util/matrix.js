081b39f965da46926354501130e04a096f083306
Julian Kaping <julian.kaping@googlemail.com>
2018-02-06 17:37:40 +0100

    removed semicolons from js-code

--- 01_NeuralNetwork/util/matrix.js
+++ 01_NeuralNetwork/util/matrix.js
@@ -1,60 +1,60 @@
 class Matrix {
 
   constructor(rows, cols) {
-    this.rows = rows;
-    this.cols = cols;
-    this.data = [];
+    this.rows = rows
+    this.cols = cols
+    this.data = []
 
     for (let i = 0; i < rows; i++) {
-      let tmp = [];
+      let tmp = []
       for (let j = 0; j < cols; j++) {
-        tmp.push(0);
+        tmp.push(0)
       }
-      this.data.push(tmp);
+      this.data.push(tmp)
     }
   }
 
   static fromArray(array) {
-    let result = new Matrix(array.length, 1);
+    let result = new Matrix(array.length, 1)
     for (let i = 0; i < array.length; i++) {
-      result.data[i][0] = array[i];
+      result.data[i][0] = array[i]
     }
-    return result;
+    return result
   }
 
   static add(a, b) {
     if (!((a instanceof Matrix) && (b instanceof Matrix))) {
-      throw new Error('A and B need to have type Matrix!');
+      throw new Error('A and B need to have type Matrix!')
     }
 
     if (a.rows != b.rows || a.cols != b.cols) {
-      throw new Error('A and B need to have the same dimensions!');
+      throw new Error('A and B need to have the same dimensions!')
     }
 
-    let result = new Matrix(a.rows, a.cols);
+    let result = new Matrix(a.rows, a.cols)
     for (let i = 0; i < a.rows; i++) {
       for (let j = 0; j < a.cols; j++) {
-        result.data[i][j] = a.data[i][j] + b.data[i][j];
+        result.data[i][j] = a.data[i][j] + b.data[i][j]
       }
     }
-    return result;
+    return result
   }
 
   static transpose(a) {
     if (!(a instanceof Matrix)) {
-      throw new Error('A needs to have type Matrix"');
+      throw new Error('A needs to have type Matrix"')
     }
 
-    let result = new Matrix(a.cols, a.rows);
+    let result = new Matrix(a.cols, a.rows)
     for (let i = 0; i < result.rows; i++) {
       for (let j = 0; j < result.cols; j++) {
-        result.data[i][j] = a.data[j][i];
+        result.data[i][j] = a.data[j][i]
       }
     }
-    return result;
+    return result
   }
 }
 
 
 
-module.exports = Matrix;
+module.exports = Matrix
